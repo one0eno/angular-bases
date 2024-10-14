f45077c5ad382e6026ea05b6ea003ae31465e600
jorge <jorge.arranz@gmail.com>
2024-10-14 21:40:04 +0200

    Avances

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { CounterModule } from './counter/counter.module';
 
 import { AppComponent } from './app.component';
 import { HeroComponent } from './heroes/hero/hero.component';
 import { ListComponent } from './heroes/list/list.component';
+import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
-  declarations: [AppComponent, HeroComponent, ListComponent],
-  imports: [BrowserModule, AppRoutingModule, CounterModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HeroesModule, DbzModule],
   providers: [],
   bootstrap: [AppComponent],
 })
